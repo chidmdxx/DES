68de09836098d74c33a3dff68541bfe341bcd4d9
chidmdxx <rachid@outlook.com>
2014-03-08 13:47:02 -0600

    sbox2

--- DES/DES/Model/DES.cs
+++ DES/DES/Model/DES.cs
@@ -217,7 +217,177 @@ namespace DES.Model
             return toReturn;
         }
 
+        private BitArray Sbox2(BitArray bits)
+        {
+            if (bits.Length != 6)
+            {
+                return null;
+            }
+            BitArray toReturn = null;
+            int inner = bits.GetInnerBits().ToInt();
 
+            if (!bits.Bit(1) && !bits.Bit(6))
+            {
+                switch (inner)
+                {
+                    case 0: toReturn = new BitArray(BitConverter.GetBytes(15));
+                        break;
+                    case 1: toReturn = new BitArray(BitConverter.GetBytes(1));
+                        break;
+                    case 2: toReturn = new BitArray(BitConverter.GetBytes(8));
+                        break;
+                    case 3: toReturn = new BitArray(BitConverter.GetBytes(14));
+                        break;
+                    case 4: toReturn = new BitArray(BitConverter.GetBytes(6));
+                        break;
+                    case 5: toReturn = new BitArray(BitConverter.GetBytes(11));
+                        break;
+                    case 6: toReturn = new BitArray(BitConverter.GetBytes(3));
+                        break;
+                    case 7: toReturn = new BitArray(BitConverter.GetBytes(4));
+                        break;
+                    case 8: toReturn = new BitArray(BitConverter.GetBytes(9));
+                        break;
+                    case 9: toReturn = new BitArray(BitConverter.GetBytes(7));
+                        break;
+                    case 10: toReturn = new BitArray(BitConverter.GetBytes(2));
+                        break;
+                    case 11: toReturn = new BitArray(BitConverter.GetBytes(13));
+                        break;
+                    case 12: toReturn = new BitArray(BitConverter.GetBytes(12));
+                        break;
+                    case 13: toReturn = new BitArray(BitConverter.GetBytes(0));
+                        break;
+                    case 14: toReturn = new BitArray(BitConverter.GetBytes(5));
+                        break;
+                    case 15: toReturn = new BitArray(BitConverter.GetBytes(10));
+                        break;
+                    default: return null;
+                }
+            }
+            if (!bits.Bit(1) && bits.Bit(6))
+            {
+                switch (inner)
+                {
+                    case 0: toReturn = new BitArray(BitConverter.GetBytes(3));
+                        break;
+                    case 1: toReturn = new BitArray(BitConverter.GetBytes(13));
+                        break;
+                    case 2: toReturn = new BitArray(BitConverter.GetBytes(4));
+                        break;
+                    case 3: toReturn = new BitArray(BitConverter.GetBytes(7));
+                        break;
+                    case 4: toReturn = new BitArray(BitConverter.GetBytes(15));
+                        break;
+                    case 5: toReturn = new BitArray(BitConverter.GetBytes(2));
+                        break;
+                    case 6: toReturn = new BitArray(BitConverter.GetBytes(8));
+                        break;
+                    case 7: toReturn = new BitArray(BitConverter.GetBytes(14));
+                        break;
+                    case 8: toReturn = new BitArray(BitConverter.GetBytes(12));
+                        break;
+                    case 9: toReturn = new BitArray(BitConverter.GetBytes(0));
+                        break;
+                    case 10: toReturn = new BitArray(BitConverter.GetBytes(1));
+                        break;
+                    case 11: toReturn = new BitArray(BitConverter.GetBytes(10));
+                        break;
+                    case 12: toReturn = new BitArray(BitConverter.GetBytes(6));
+                        break;
+                    case 13: toReturn = new BitArray(BitConverter.GetBytes(9));
+                        break;
+                    case 14: toReturn = new BitArray(BitConverter.GetBytes(11));
+                        break;
+                    case 15: toReturn = new BitArray(BitConverter.GetBytes(5));
+                        break;
+                    default: return null;
+                }
+            }
+
+            if (bits.Bit(1) && !bits.Bit(6))
+            {
+                switch (inner)
+                {
+                    case 0: toReturn = new BitArray(BitConverter.GetBytes(0));
+                        break;
+                    case 1: toReturn = new BitArray(BitConverter.GetBytes(14));
+                        break;
+                    case 2: toReturn = new BitArray(BitConverter.GetBytes(7));
+                        break;
+                    case 3: toReturn = new BitArray(BitConverter.GetBytes(11));
+                        break;
+                    case 4: toReturn = new BitArray(BitConverter.GetBytes(10));
+                        break;
+                    case 5: toReturn = new BitArray(BitConverter.GetBytes(4));
+                        break;
+                    case 6: toReturn = new BitArray(BitConverter.GetBytes(13));
+                        break;
+                    case 7: toReturn = new BitArray(BitConverter.GetBytes(1));
+                        break;
+                    case 8: toReturn = new BitArray(BitConverter.GetBytes(5));
+                        break;
+                    case 9: toReturn = new BitArray(BitConverter.GetBytes(8));
+                        break;
+                    case 10: toReturn = new BitArray(BitConverter.GetBytes(12));
+                        break;
+                    case 11: toReturn = new BitArray(BitConverter.GetBytes(6));
+                        break;
+                    case 12: toReturn = new BitArray(BitConverter.GetBytes(9));
+                        break;
+                    case 13: toReturn = new BitArray(BitConverter.GetBytes(3));
+                        break;
+                    case 14: toReturn = new BitArray(BitConverter.GetBytes(2));
+                        break;
+                    case 15: toReturn = new BitArray(BitConverter.GetBytes(15));
+                        break;
+                    default: return null;
+                }
+            }
+
+            if (bits.Bit(1) && bits.Bit(6))
+            {
+                switch (inner)
+                {
+                    case 0: toReturn = new BitArray(BitConverter.GetBytes(13));
+                        break;
+                    case 1: toReturn = new BitArray(BitConverter.GetBytes(8));
+                        break;
+                    case 2: toReturn = new BitArray(BitConverter.GetBytes(10));
+                        break;
+                    case 3: toReturn = new BitArray(BitConverter.GetBytes(1));
+                        break;
+                    case 4: toReturn = new BitArray(BitConverter.GetBytes(3));
+                        break;
+                    case 5: toReturn = new BitArray(BitConverter.GetBytes(15));
+                        break;
+                    case 6: toReturn = new BitArray(BitConverter.GetBytes(4));
+                        break;
+                    case 7: toReturn = new BitArray(BitConverter.GetBytes(2));
+                        break;
+                    case 8: toReturn = new BitArray(BitConverter.GetBytes(11));
+                        break;
+                    case 9: toReturn = new BitArray(BitConverter.GetBytes(6));
+                        break;
+                    case 10: toReturn = new BitArray(BitConverter.GetBytes(7));
+                        break;
+                    case 11: toReturn = new BitArray(BitConverter.GetBytes(12));
+                        break;
+                    case 12: toReturn = new BitArray(BitConverter.GetBytes(0));
+                        break;
+                    case 13: toReturn = new BitArray(BitConverter.GetBytes(5));
+                        break;
+                    case 14: toReturn = new BitArray(BitConverter.GetBytes(14));
+                        break;
+                    case 15: toReturn = new BitArray(BitConverter.GetBytes(9));
+                        break;
+                    default: return null;
+                }
+            }
+
+            toReturn.Length = 4;
+            return toReturn;
+        }
 
     }
 }
