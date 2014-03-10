d18e44c2df471b50d94e6fe6f369d6f5914eb2dd
chidmdxx <rachid@outlook.com>
2014-03-10 16:27:08 -0600

    algunos comentarios

--- DES/DES/Model/Helper.cs
+++ DES/DES/Model/Helper.cs
@@ -9,12 +9,12 @@ namespace DES.Model
 {
     static class Helper
     {
-        public static bool Bit (this BitArray bits, int index)
+        public static bool Bit (this BitArray bits, int index) //hace que la little endian class de BitArray se lea como big endian
         {
             return bits[bits.Length - index];
         }
 
-        public static void Bit(this BitArray bits, int index, bool value)
+        public static void Bit(this BitArray bits, int index, bool value) //asigna valores como big endian
         {
             bits[bits.Length - index] = value;
         }
@@ -22,7 +22,7 @@ namespace DES.Model
         public static byte[] ToByteArray(this BitArray bits)
         {
             int numBytes = bits.Count / 8;
-            if (bits.Count % 8 != 0) numBytes++;
+            if (bits.Count % 8 != 0) numBytes++; //evita que sea divisible entre 0
 
             byte[] bytes = new byte[numBytes];
             int byteIndex = 0, bitIndex = 0;
@@ -30,7 +30,7 @@ namespace DES.Model
             for (int i = 0; i < bits.Count; i++)
             {
                 if (bits[i])
-                    bytes[byteIndex] |= (byte)(1 << (7 - bitIndex));
+                    bytes[byteIndex] |= (byte)(1 << (7 - bitIndex)); //arma los bytes
 
                 bitIndex++;
                 if (bitIndex == 8)
@@ -55,7 +55,7 @@ namespace DES.Model
 
         public static void ShiftLeft(this BitArray bits)
         {
-            bool first = bits.Bit(1);
+            bool first = bits.Bit(1); //guardar el primero
             for (var i=2;i<=bits.Length;i++)
             {
                 bits[i - 1] = bits[i];
