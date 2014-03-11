0d10fbeb093d5dfdb3dd9745859b6191c27da3e0
chidmdxx <rachid@outlook.com>
2014-03-11 00:14:05 -0600

    unit test con el pdf de clase para hex

--- DES/DESTesting/UnitTest1.cs
+++ DES/DESTesting/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DES;
+using DES.Model;
 
 namespace DESTesting
 {
@@ -10,7 +10,15 @@ namespace DESTesting
         [TestMethod]
         public void TestMethod1()
         {
+            string M = "0123456789ABCDEF";
+            string key = "133457799BBCDFF1";
+            var test = new DESClass();
+            test.Plaintext = M;
+            test.Key = key;
+            test.CipherHex(16);
 
+            Assert.AreEqual("85E813540F0AB405", test.Ciphertext, true);
+            int a = 0;
         }
     }
 }
