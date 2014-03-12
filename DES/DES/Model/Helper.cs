8437b1709411d9f1d5f553d398dd8627c1f75b2a
chidmdxx <rachid@outlook.com>
2014-03-12 10:37:03 -0600

    formatting

--- DES/DES/Model/Helper.cs
+++ DES/DES/Model/Helper.cs
@@ -128,7 +128,9 @@ namespace DES.Model
             int NumberChars = hex.Length;
             byte[] bytes = new byte[NumberChars / 2];
             for (int i = 0; i < NumberChars; i += 2)
+            {
                 bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
             return bytes;
         }
 
