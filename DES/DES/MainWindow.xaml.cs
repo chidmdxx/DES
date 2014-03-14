c1bb9b669d9b4bc25f0d7d4dad8fd6d1f87b8f27
chidmdxx <rachid@outlook.com>
2014-03-14 00:50:58 -0600

    valores random de llave y multiples rondas para mensajes mas largos

--- DES/DES/MainWindow.xaml.cs
+++ DES/DES/MainWindow.xaml.cs
@@ -24,54 +24,157 @@ namespace DES
         public MainWindow()
         {
             InitializeComponent();
+            messagebox.Focus();
         }
 
         private void cipherButton_Click(object sender, RoutedEventArgs e)
         {
             string plaintext = messagebox.Text;
             string key = keybox.Text;
+            string ciphertext = string.Empty;
+            string works = string.Empty;
+            string send = string.Empty;
             bool hex = (bool)hexRadio.IsChecked;
             int round = roundBox.SelectedIndex + 1;
             if (hex)
             {
-                plaintext.Substring(0, 16);
-                key.Substring(0, 16);
-                var des = new DESClass() { Plaintext = plaintext, Key = key };
-                des.CipherHex(round);
-                workText.Text = string.Format("{0}{1} CipherBytes: {2}",des.Work,Environment.NewLine, des.Ciphertext);
+                for (int i = 0; i < plaintext.Length; i += 16)
+                {
+                    if (plaintext.Length - i > 16)
+                    {
+                        send = plaintext.Substring(i, 16);
+                    }
+                    else
+                    {
+                        send = plaintext.Substring(i, plaintext.Length - i);
+                    }
+                    if (send.Length != 16)
+                    {
+                        int missing = 16 - send.Length;
+                        for (int j = 0; j < missing; j++)
+                        {
+                            send += "0";
+                        }
+                    }
+                    works += string.Format("Part {0}{1}", i, Environment.NewLine);
+                    var des = new DESClass() { Plaintext = send, Key = key };
+                    des.CipherHex(round);
+                    works += string.Format("{0}{1}{1}", des.Work, Environment.NewLine);
+                    ciphertext += des.Ciphertext;
+                }
+
             }
             else
             {
-                plaintext.Substring(0, 8);
-                key.Substring(0, 8);
-                var des = new DESClass() { Plaintext = plaintext, Key = key };
-                des.CipherString(round);
-                workText.Text = string.Format("{0}{1} CipherBytes: {2}", des.Work, Environment.NewLine, des.Ciphertext);
+                for (int i = 0; i < plaintext.Length; i += 8)
+                {
+                    if (plaintext.Length - i > 8)
+                    {
+                        send = plaintext.Substring(i, 8);
+                    }
+                    else
+                    {
+                        send = plaintext.Substring(i, plaintext.Length - i);
+                    }
+                    if (send.Length != 8)
+                    {
+                        int missing = 8 - send.Length;
+                        for (int j = 0; j < missing; j++)
+                        {
+                            send += "0";
+                        }
+                    }
+                    works += string.Format("Part {0}{1}", i, Environment.NewLine);
+                    var des = new DESClass() { Plaintext = send, Key = key };
+                    des.CipherString(round);
+                    works += string.Format("{0}{1}{1}", des.Work, Environment.NewLine);
+                    ciphertext += des.Ciphertext;
+                }
+
             }
+            workText.Text = string.Format("{0}{1} CipherBytes: {2}", works, Environment.NewLine, ciphertext);
         }
 
         private void decipherButton_Click(object sender, RoutedEventArgs e)
         {
             string ciphertext = messagebox.Text;
             string key = keybox.Text;
+            string plaintext = string.Empty;
+            string works = string.Empty;
+            string send = string.Empty;
             bool hex = (bool)hexRadio.IsChecked;
             int round = roundBox.SelectedIndex + 1;
             if (hex)
             {
-                ciphertext.Substring(0, 16);
-                key.Substring(0, 16);
-                var des = new DESClass() { Ciphertext = ciphertext, Key = key };
-                des.DecipherHex(round);
-                workText.Text = string.Format("{0}{1} PlainBytes: {2}", des.Work, Environment.NewLine, des.Plaintext);
+                for (int i = 0; i < ciphertext.Length; i += 16)
+                {
+                    if (ciphertext.Length - i > 16)
+                    {
+                        send = ciphertext.Substring(i, 16);
+                    }
+                    else
+                    {
+                        send = ciphertext.Substring(i, plaintext.Length - i);
+                    }
+                    if (send.Length != 16)
+                    {
+                        int missing = 16 - send.Length;
+                        for (int j = 0; j < missing; j++)
+                        {
+                            send += "0";
+                        }
+                    }
+                    works += string.Format("Part {0}{1}", i, Environment.NewLine);
+                    var des = new DESClass() { Ciphertext = send, Key = key };
+                    des.DecipherHex(round);
+                    works += string.Format("{0}{1}{1}", des.Work, Environment.NewLine);
+                    plaintext += des.Plaintext;
+                }
+               
             }
             else
             {
-                ciphertext.Substring(0, 8);
-                key.Substring(0, 8);
-                var des = new DESClass() { Ciphertext = ciphertext, Key = key };
-                des.DecipherString(round);
-                workText.Text = string.Format("{0}{1} PlainBytes: {2}", des.Work, Environment.NewLine, des.Plaintext);
+                for (int i = 0; i < ciphertext.Length; i += 16)
+                {
+                    if (ciphertext.Length - i > 16)
+                    {
+                        send = ciphertext.Substring(i, 16);
+                    }
+                    else
+                    {
+                        send = ciphertext.Substring(i, ciphertext.Length - i);
+                    }
+                    if (send.Length != 16)
+                    {
+                        int missing = 16 - send.Length;
+                        for (int j = 0; j < missing; j++)
+                        {
+                            send += "0";
+                        }
+                    }
+                    works += string.Format("Part {0}{1}", i, Environment.NewLine);
+                    var des = new DESClass() { Ciphertext = send, Key = key };
+                    des.DecipherString(round);
+                    works += string.Format("{0}{1}{1}", des.Work, Environment.NewLine);
+                    plaintext += des.Plaintext;
+                }
             }
+            workText.Text = string.Format("{0}{1} PlainBytes: {2}", works, Environment.NewLine, plaintext);
+        }
+
+        private void randomKey_Click(object sender, RoutedEventArgs e)
+        {
+            string key = string.Empty;
+            char[] possible = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            Random random = new Random();
+
+            int posicion;
+            for (var j = 0; j < 16; j++)
+            {
+                posicion = random.Next(16);
+                key += possible[posicion];
+            }
+            keybox.Text = key;
         }
     }
 }
